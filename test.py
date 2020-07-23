b0a8c70b49cd15a06477f3dc2b7b8554ff16af1a
Thomas <thombsaway@gmail.com>
2020-07-23 13:48:20 +1000

    Added function getExerciseMaxAverage
    Which:
    ''Computes the average session maximum weight lifted for the given exercise and rep number.''
    
    For each unique date in the dataset this function:
        - gets all sets with that date
        - gets all sets matching the parameter exercise
        - gets all sets matching the parameter reps
        - gets the set with the highest weight for that date
        - appends that weight to a list
    
    After all unique dates are run through, the list of max weights is
    averaged, and returned.
    
    It's a bit spagetti at the moment, but it's parameterised so it can run
    for any exercise and rep number.

--- test.py
+++ test.py
@@ -3,10 +3,53 @@
 import pandas
 
 import os 
+import statistics
 
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-# logging.disable(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
 
 logging.debug('cwd: %s' % (os.getcwd()))
 dataset = pandas.read_csv('lifting_data.csv')
+
+
+def getExerciseMaxAverage(exercise: str, reps: int) -> float:
+    '''Computes the average session maximum weight lifted for the given exercise and rep number.'''
+
+    exercise_maxes = []
+
+    for date in dataset['Date'].unique():
+
+        logging.debug('Exercise being searched: %s' % (exercise))
+
+        # get the sets for this date
+        sets = dataset[dataset['Date'] == date]
+        #logging.debug('sets: %s' % (sets))
+
+        # get any snatch pull sets that happened on this date
+        exercise_sets = sets[sets['Exercise'] == exercise]
+        logging.debug('exercise_sets: \n%s' % (exercise_sets))
+
+        # we want only the sets with reps of 3
+        exercise_sets_reps = exercise_sets[exercise_sets['Reps'] == reps]
+        logging.debug('exercise_sets_reps: \n%s' % (exercise_sets_reps))
+
+        # we want to add only the highest snatch pull weight
+        exercise_max = exercise_sets_reps['Weight'].max()
+        logging.debug('exercise_max: \n%s' % (exercise_max))
+
+        exercise_maxes.append(exercise_max)
+
+    clean_maxes = [x for x in exercise_maxes if str(x) != 'nan']
+    logging.debug('exercise_maxes: \n%s' % (clean_maxes))
+
+    exercise_average = statistics.mean(clean_maxes)
+    logging.debug('exercise_average: \n%s' % (exercise_average))
+
+    return exercise_average
+
+print('Snatch: %s' % (getExerciseMaxAverage('Snatch', 3)))
+print('Clean: %s' % (getExerciseMaxAverage('Clean', 3)))
+print('Clean and Jerk: %s' % (getExerciseMaxAverage('Clean and Jerk', 3)))
+print('Power Snatch: %s' % (getExerciseMaxAverage('Power Snatch', 3)))
+print('Power Clean: %s' % (getExerciseMaxAverage('Power Clean', 3)))
