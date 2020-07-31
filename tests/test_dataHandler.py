65c87532459f92c8702281497b9f2866d3506013
Thomas <thombsaway@gmail.com>
2020-07-31 16:00:04 +1000

    Add logging to test modules

--- tests/test_dataHandler.py
+++ tests/test_dataHandler.py
@@ -1,34 +1,36 @@
 #! python3
 
+# Imports
 from data.dataHandler import DataHandler
+
 import pandas
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 testDataFrame = pandas.DataFrame({'Date': ['2020-06-01', '2020-06-01', '2020-06-01', '2020-06-01', '2020-06-01'], 'Exercise': ['Snatch', 'Snatch', 'Snatch', 'Snatch', 'Snatch'], 'Reps': [1, 2, 3, 4, 5], 'Weight': [10, 20, 30, 40, 50]})
 
 class Test_DataHandler():
 
     def setup_method(self):
+        logging.info('Setting up before test... ')
         self.dh = DataHandler()
     
     def teardown_method(self):
+        logging.info('Tearing down after test... ')
         self.dh = None
 
     def test_getExercises(self):
-
         exercises = ['Snatch']
         assert self.dh.getExercises(testDataFrame) == exercises
 
     def test_getExerciseMaxAverage(self):
-
         exercise, rep, weight = 'Snatch', 5, 50.0
         assert self.dh.getExerciseMaxAverage(testDataFrame, exercise, rep) == weight
 
     def test_getExerciseMax(self):
-
         exercise, rep, weight = 'Snatch', 1, 10.0
         assert self.dh.getExerciseMax(testDataFrame, exercise, rep) == weight
 
     def test_getExerciseMaxes(self):
-
         exercise, rep, weight = 'Snatch', 5, 50.0
         assert self.dh.getExerciseMaxAverage(testDataFrame, exercise, rep) == weight
--- tests/test_dataReader.py
+++ tests/test_dataReader.py
@@ -1,25 +1,28 @@
 #! python3
 
+# Imports
 from data.dataReader import DataReader
 from data.dataHandler import DataHandler
 
-import pandas
 from datetime import datetime
+import pandas
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Test_DataReader():
 
     def setup_method(self):
+        logging.info('Setting up before test... ')
         self.dr = DataReader('tests\\test_data.csv')
     
     def teardown_method(self):
+        logging.info('Tearing down after test... ')
         self.dr = None
 
     def test_dataReaderInit(self):
-        #dr = DataReader('tests\\test_data.csv')
         assert self.dr != None
 
     def test_dataReaderContent(self):
-        #dr = DataReader('tests\\test_data.csv')
         expected = pandas.DataFrame({'Date': ['2019-04-06'], 'Exercise': ['Snatch'], 'Reps': [1], 'Weight': [68]})
         actual = self.dr.getData()
 
