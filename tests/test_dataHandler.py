1eb2c5cba90048c6cde37748fe8066b88a91f2d7
Thomas <thombsaway@gmail.com>
2020-07-31 12:46:04 +1000

    Create tests for dataHandler

--- tests/test_dataHandler.py
+++ tests/test_dataHandler.py
@@ -3,9 +3,28 @@
 from data.dataHandler import dataHandler
 import pandas
 
-def test_getExerciseMax():
-    testDataFrame = pandas.DataFrame({'Date': ['2020-06-01', '2020-06-01', '2020-06-01', '2020-06-01', '2020-06-01'], 'Exercise': ['Snatch', 'Snatch', 'Snatch', 'Snatch', 'Snatch'], 'Reps': [1, 2, 3, 4, 5], 'Weight': [10, 20, 30, 40, 50]})
+testDataFrame = pandas.DataFrame({'Date': ['2020-06-01', '2020-06-01', '2020-06-01', '2020-06-01', '2020-06-01'], 'Exercise': ['Snatch', 'Snatch', 'Snatch', 'Snatch', 'Snatch'], 'Reps': [1, 2, 3, 4, 5], 'Weight': [10, 20, 30, 40, 50]})
+
+def test_getExercises():
 
     dh = dataHandler()
+    exercises = ['Snatch']
+    assert dh.getExercises(testDataFrame) == exercises
 
-    assert dh.getExerciseMax(testDataFrame, 'Snatch', 1) == 10.0
+def test_getExerciseMaxAverage():
+
+    dh = dataHandler()
+    exercise, rep, weight = 'Snatch', 5, 50.0
+    assert dh.getExerciseMaxAverage(testDataFrame, exercise, rep) == weight
+
+def test_getExerciseMax():
+
+    dh = dataHandler()
+    exercise, rep, weight = 'Snatch', 1, 10.0
+    assert dh.getExerciseMax(testDataFrame, exercise, rep) == weight
+
+def test_getExerciseMaxes():
+
+    dh = dataHandler()
+    exercise, rep, weight = 'Snatch', 5, 50.0
+    assert dh.getExerciseMaxAverage(testDataFrame, exercise, rep) == weight
