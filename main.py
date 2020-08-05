92fbed70203a176c9b9f69ac528cb1a1d2525a53
Thomas <thombsaway@gmail.com>
2020-08-05 11:47:43 +1000

    Added print for average max by exercise

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ for i in range(len(competitionDates)):
     print()
     print('Lifts on competition dates: \n{}'.format(dh.getSets(dr.getData(), fromDate=competitionDates[i], toDate=competitionDates[i])))
 
+print()
 print('Training period data')
 for i in range(len(trainingPeriods)):
 
@@ -34,4 +35,15 @@ for i in range(len(trainingPeriods)):
     print('For training period {}:{}'.format(datetime.strftime(trainingPeriods[i][0], '%Y-%m-%d'), datetime.strftime(trainingPeriods[i][1], '%Y-%m-%d')))
 
     for exercise in dh.getExercises(periodData):
+        print('Average max weight for {} 3s was: {}'.format(exercise, dh.getExerciseMaxAverage(periodData, exercise, 3)))
+
+for exercise in dh.getExercises(allData):
+
+    print()
+    print('The average max sets of 3 for {}, for each training period:'.format(exercise))    
+
+    for i in range(len(trainingPeriods)):
+        periodData = dr.getData(trainingPeriods[i][0], trainingPeriods[i][1])
+
+        print('For training period {}:{}'.format(datetime.strftime(trainingPeriods[i][0], '%Y-%m-%d'), datetime.strftime(trainingPeriods[i][1], '%Y-%m-%d')))
         print('Average max weight for {} 3s was: {}'.format(exercise, dh.getExerciseMaxAverage(periodData, exercise, 3)))
