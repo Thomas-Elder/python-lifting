91ece774598d5770874a40e42565f490d51ce0c4
Thomas <thombsaway@gmail.com>
2020-08-05 11:43:19 +1000

    Add automated print statements for training periods

--- data/dataHandler.py
+++ data/dataHandler.py
@@ -9,7 +9,7 @@ from datetime import datetime, timedelta
 
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-#logging.disable(logging.CRITICAL)
+logging.disable(logging.CRITICAL)
 
 class DataHandler:
 
--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 from data.dataReader import DataReader
 from data.dataHandler import DataHandler
+
+from datetime import datetime
 import pandas
 
 dr = DataReader('data\\data.csv')
@@ -19,5 +21,15 @@ print()
 print('Dates of competition:{}'.format(competitionDates))
 print('Training periods: {}'.format(trainingPeriods))
 
-print('len(competitionDates): {}'.format(len(competitionDates)))
-print('Lifts on competition dates: \n{}'.format(dh.getExercises(dr.getData(fromDate=competitionDates[0], toDate=competitionDates[0]))))
+for i in range(len(competitionDates)):
+    print()
+    print('Lifts on competition dates: \n{}'.format(dh.getSets(dr.getData(), fromDate=competitionDates[i], toDate=competitionDates[i])))
+
+print('Training period data')
+for i in range(len(trainingPeriods)):
+
+    print()
+    print('For training period {}:{}'.format(datetime.strftime(trainingPeriods[i][0], '%Y-%m-%d'), datetime.strftime(trainingPeriods[i][1], '%Y-%m-%d')))
+
+    for exercise in dh.getExercises(dr.getData(trainingPeriods[i][0], trainingPeriods[i][1])):
+        print('Average max weight for {} 3s was: {}'.format(exercise, dh.getExerciseMaxAverage(dr.getData(trainingPeriods[i][0], trainingPeriods[i][1]), exercise, 3)))
