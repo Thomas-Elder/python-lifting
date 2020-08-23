22812d7122a037607d147e99c25dcb2d9752e550
Thomas Elder <thombsaway@gmail.com>
2020-08-23 13:43:37 +1000

    Update data.csv and start updating main

--- main.py
+++ main.py
@@ -7,32 +7,31 @@ from datetime import datetime
 import pandas
 
 dr = DataReader('data\\data.csv')
-
 dh = DataHandler()
 
 print()
 print('Lets get real data now... ')
 
-allData = dr.getData()
+allData = dr.sessions
 competitionDates = dh.getCompetitionDates(allData)
-trainingPeriods = dh.getPeriodDates(allData, competitionDates)
+trainingPeriods = dh.getPeriodDates(competitionDates)
 
 print()
 print('Dates of competition:{}'.format(competitionDates))
 print('Training periods: {}'.format(trainingPeriods))
 
-for i in range(len(competitionDates)):
-    print()
-    print('Lifts on competition dates: \n{}'.format(dh.getSets(dr.getData(), fromDate=competitionDates[i], toDate=competitionDates[i])))
+print()
+for date in competitionDates:  
+    print('Lifts on competition dates: \n{}'.format(dh.getSessions(allData, fromDate=date, toDate=date)))
 
 print()
 print('Training period data')
-for i in range(len(trainingPeriods)):
+for period in trainingPeriods:
 
-    periodData = dr.getData(trainingPeriods[i][0], trainingPeriods[i][1])
+    periodData = dh.getSessions(allData, period[0], period[1])
 
     print()
-    print('For training period {}:{}'.format(datetime.strftime(trainingPeriods[i][0], '%Y-%m-%d'), datetime.strftime(trainingPeriods[i][1], '%Y-%m-%d')))
+    print('For training period {}:{}'.format(datetime.strftime(period[0], '%Y-%m-%d'), datetime.strftime(period[1], '%Y-%m-%d')))
 
     for exercise in dh.getExercises(periodData):
 
