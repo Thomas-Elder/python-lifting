09ab1c0ecbb22ae4924c4b5e00daee9768f8f0f7
Thomas <thombsaway@gmail.com>
2020-10-22 14:10:25 +1100

    Test.py to call datareader and time functions

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 from timeit import default_timer as timer
 
 from data.dataReader import DataReader
+from data.dataReader_Sort import DataReader_Sort
 from data.dataHandler import DataHandler
 
 start = timer()
-dr = DataReader('data\\data.csv')
+#dr = DataReader('data\\data.csv')
 end = timer()
 
 print(f'Data reader init exection time: {round(end-start, 2)} seconds') #  5.98 seconds
+
+start = timer()
+dr = DataReader_Sort('data\\data.csv')
+end = timer()
+
+print(f'Data reader sort init exection time: {round(end-start, 2)} seconds') #  5.98 seconds
